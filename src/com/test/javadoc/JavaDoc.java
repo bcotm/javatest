c5cc1b67837902f78e6315bc2f47b0e210f756bb
samclwang <samclwang@tencent.com>
2019-03-07 19:27:41 +0800

    java文档注释

---
+++ src/com/test/javadoc/JavaDoc.java
@@ -0,0 +1,18 @@
+package com.test.javadoc;
+
+/**
+ * 这个是类的注释
+ *
+ * 这里描述类的详细介绍，跟git commit comment一样
+ */
+public class JavaDoc {
+    /**
+     * 这个是方法的注释
+     * @author 作者
+     * @param paramname 参数的注释
+     * @return 返回值说明
+     */
+    public void whatMethod(String paramname){
+
+    }
+}
