c49f4d71467300c4195e6512155f0787c1ba343a
samclwang <samclwang@tencent.com>
2019-03-07 13:27:35 +0800

    java对象的equals()和hashCode()方法
    在使用哈希集合类时一定要重载这个两个方法，equals()为逻辑上对象相同的判别方法，hashCode()则是先进行判断的，默认是对象的地址

---
+++ src/com/test/object/equalsandhashcode/Student.java
@@ -0,0 +1,63 @@
+package com.test.object.equalsandhashcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Student {
+    private String id;
+    private String name;
+
+    Student(String id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object other){
+       if(other == null){
+           return false;
+       }
+       if( this == other){
+           return true;
+       }
+       if(other instanceof Student){
+           Student otherStudent = (Student) other;
+           if(otherStudent.id == id){
+               return true;
+           }
+           return false;
+       }
+       return false;
+    }
+
+    @Override
+    public int hashCode(){
+        return id.hashCode();
+    }
+
+    public static void main(String[] args){
+        /*
+        如果不Override equals和hashCode方法
+        * */
+        Map<Student, Integer> mathScore = new HashMap<>();
+        mathScore.put(new Student("123", "小明"), 99);
+        mathScore.put(new Student("123", "小明"), 100);
+
+        /*
+        未重载或只重载equals：
+        小明
+        123
+        小明
+        123
+        * */
+        /*
+        重载：
+        小明
+        123
+        * */
+        for( Student student : mathScore.keySet()){
+            System.out.println(student.name);
+            System.out.println(student.id);
+        }
+    }
+}
